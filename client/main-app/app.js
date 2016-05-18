195d7aa7a33a0707a50567f20f28872c3375859b
Pokegear <christoph.ding@gmail.com>
2016-05-17 21:26:40 -0400

    Put in stubs for different pages.  Starting on partials now.

--- client/main-app/app.js
+++ client/main-app/app.js
@@ -1,1 +1,20 @@
 var app = angular.module('app', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/signin');
+
+  $stateProvider
+    // Sign in
+    .state('signin', {
+      url: '/signin',
+      template: 'sign in'
+    })
+    .state('quiz', {
+      url: '/quiz',
+      template: 'quiz'
+    })
+    .state('admin', {
+      url: '/admin',
+      template: 'admin'
+    })
+})
