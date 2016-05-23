5633aa9b7653da7a50999988d8a1c82f4e26a287
Pokegear <christoph.ding@gmail.com>
2016-05-22 22:34:51 -0400

    Working on the review game

--- client/sections/review/review.js
+++ client/sections/review/review.js
@@ -4,6 +4,9 @@ review.controller('reviewCtrl', function ($scope, adminFunctionality) {
 
   $scope.cards;
   $scope.currentCard;
+  $scope.currentIndex = 0;
+  $scope.seenAllZeroes = false;
+  $scope.seenAllWithinTime = false;
 
   $scope.fetchCards = function() {
     adminFunctionality.fetchCards()
@@ -11,7 +14,11 @@ review.controller('reviewCtrl', function ($scope, adminFunctionality) {
       return $scope.cards = cards;
     })
     .then(function () {
-      return $scope.reviewTime();
+      // sort the deck by bin
+      $scope.cards.sort(function (a,b) {
+        return b.bin - a.bin;
+      });
+      return;
     })
   }
 
@@ -23,38 +30,86 @@ review.controller('reviewCtrl', function ($scope, adminFunctionality) {
     }
   }
 
-  $scope.reviewCard = function (card) {
-    // change card
-    $scope.currentCard = card;
-
-  }
-
-
   // only show cards that have elapsed enough time, based on bin
-  $scope.checkTime = function (card) {
+  $scope.timeToShow = function (card) {
     timeDifference = new Date(card['next']) - new Date();
     return timeDifference <= 0;
   }
 
-  // review controller
-  $scope.reviewTime = function() {
-    console.log('inside game');
-
-    // 1. sort the deck by bin
-    $scope.cards.sort(function (a,b) {
-      return b.bin - a.bin;
-    })
-    console.log('sorted deck is: ', $scope.cards);
-    // 2. review all the zero bin cards
-
-    $scope.goThroughDeck(function (card) {
-      console.log($scope.checkTime(card));            
-      if (card['bin'] === 0 && $scope.checkTime(card)) {
-        // show the card
+  // play the game;
+  $scope.findNextZero = function () {
+    // 'iterate' through the deck until we find a zero bin card
+    while ($scope.currentIndex < $scope.cards.length) {
+      if (currentCard['bin'] === 0) {
+        $scope.currentCard = currentCard;
+        return $scope.currentIndex++;
       }
-    });
+      $scope.currentIndex++;
+    }
+    // upon reaching the end, we no longer explicitely look for zero bin cards
+    return $scope.seenAllZeroes = true;
   }
 
+  $scope.chooseWhateverCard = function () {
+    // 'iterate' through the deck until we find a zero bin card
+    while ($scope.currentIndex < $scope.cards.length) {
+      // show cards that are within the time frame
+
+      
+    }
+  }
+
+
+
+  // find next card  
+  $scope.chooseNextCard = function() {
+    // 'iterate' through the deck until we find the appropriate card
+    while ($scope.currentIndex < $scope.cards.length) {
+      var currentCard = $scope.cards[$scope.currentIndex];
+      // first, we use zero bin cards
+      if ($scope.seenAllZeroes === false) {
+        if (currentCard['bin'] === 0) {
+          // we choose the next card, then stop looking
+          $scope.currentCard = currentCard;
+          return $scope.currentIndex++;
+        }
+      } else {
+      // now that we have used all zero bin cards ...
+      // show cards that are within the time frame
+        if (timeToShow(currentCard)) {
+          $scope.currentCard = currentCard;
+          return $scope.currentIndex++;
+        }
+      }
+      $scope.currentIndex++;
+    }
+    // after iterating through the list
+    if ($scope.seenAllZeroes === false) {
+      // so we don't have the 'choose zeros first' rule anymore
+      $scope.seenAllZeroes = true;
+    } else {
+
+    }
+    return;
+  }
+
+  // // review controller
+  // $scope.reviewTime = function() {
+  //   console.log('inside game');
+
+    // 1. sort the deck by bin
+    // $scope.cards.sort(function (a,b) {
+    //   return b.bin - a.bin;
+    // });
+
+  //   // 2. review all the zero bin cards
+  //   $scope.goThroughDeck(function (card) {
+  //     if (card['bin'] === 0 && $scope.checkTime(card)) {
+  //       // show the card
+  //       $scope.reviewCard(card);
+  //     }
+  //   });
+  // }
 
   // get the cards from database
   $scope.$on('$viewContentLoaded', function(){
