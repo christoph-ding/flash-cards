46489258a1663c3fad009d5022380818d6ca66aa
Pokegear <christoph.ding@gmail.com>
2016-05-22 23:08:56 -0400

    Updates the card based on getting right or wrong.

--- client/sections/review/review.js
+++ client/sections/review/review.js
@@ -2,12 +2,28 @@ var review = angular.module('review', ['admin']);
 
 review.controller('reviewCtrl', function ($scope, adminFunctionality) {
 
+  // global variables, yuck
   $scope.cards;
   $scope.currentCard;
   $scope.currentIndex = 0;
   $scope.seenAllZeroes = false;
   $scope.seenAllWithinTime = false;
 
+  // time based on bin
+  $scope.binTimes = {
+    '1': 5,
+    '2': 25,
+    '3': 120,
+    '4': 600,
+    '5': 3600,
+    '6': 18000,
+    '7': 86400,
+    '8': 432000,
+    '9': 2160000,
+    '10': 10540800
+  };
+
+  // get cards from database
   $scope.fetchCards = function() {
     adminFunctionality.fetchCards()
     .then(function (cards) {
@@ -20,96 +36,76 @@ review.controller('reviewCtrl', function ($scope, adminFunctionality) {
       });
       return;
     })
-  }
-
-  // helper functions
-  $scope.goThroughDeck = function (callback) {
-    for (var i = 0; i < $scope.cards.length; i++) {
-      var currentCard = $scope.cards[i];
-      callback(currentCard);
-    }
-  }
+  };
 
   // only show cards that have elapsed enough time, based on bin
   $scope.timeToShow = function (card) {
     timeDifference = new Date(card['next']) - new Date();
     return timeDifference <= 0;
+  };
+
+  // calculate next time to show, based on bin
+  $scope.calculateNext = function (card) {
+    return new Date( new Date() + $scope.binTimes[card['bin']]);
   }
+
+  // "Level Up" a card / update the card based on answer 
+  $scope.updateCard = function (card, gotItRight) {
+    // user got it right
+    if (gotItRight) {
+      card['bin']++;
+      card['right']++;
+    } else {
+    // use got it wrong
+      card['bin'] = 1;
+      card['wrong']++; 
+    }
+    return $scope.calculateNext(card);
+  };
 
   // play the game;
   $scope.findNextZero = function () {
     // 'iterate' through the deck until we find a zero bin card
     while ($scope.currentIndex < $scope.cards.length) {
+      var currentCard = $scope.cards[$scope.currentIndex];
       if (currentCard['bin'] === 0) {
         $scope.currentCard = currentCard;
         return $scope.currentIndex++;
       }
       $scope.currentIndex++;
     }
+    // start over
+    $scope.currentIndex = 0;
     // upon reaching the end, we no longer explicitely look for zero bin cards
     return $scope.seenAllZeroes = true;
-  }
+  };
 
-  $scope.chooseWhateverCard = function () {
-    // 'iterate' through the deck until we find a zero bin card
-    while ($scope.currentIndex < $scope.cards.length) {
-      // show cards that are within the time frame
-
-      
-    }
-  }
-
-
-
-  // find next card  
-  $scope.chooseNextCard = function() {
-    // 'iterate' through the deck until we find the appropriate card
+  $scope.findNextWhateverCard = function () {
+    // 'iterate' through the deck until we find a card within time bound
     while ($scope.currentIndex < $scope.cards.length) {
       var currentCard = $scope.cards[$scope.currentIndex];
-      // first, we use zero bin cards
-      if ($scope.seenAllZeroes === false) {
-        if (currentCard['bin'] === 0) {
-          // we choose the next card, then stop looking
-          $scope.currentCard = currentCard;
-          return $scope.currentIndex++;
-        }
-      } else {
-      // now that we have used all zero bin cards ...
-      // show cards that are within the time frame
-        if (timeToShow(currentCard)) {
-          $scope.currentCard = currentCard;
-          return $scope.currentIndex++;
-        }
+      if ($scope.timeToShow($scope.currentCard)) {
+        $scope.currentCard = currentCard;
+        return $scope.currentIndex++;
       }
       $scope.currentIndex++;
     }
-    // after iterating through the list
+     // start over
+     $scope.currentIndex = 0;
+     // upon reaching the end, we will say we have seen all within time
+     return $scope.seenAllWithinTime = true;   
+  };
+
+  // find next card  
+  $scope.chooseNextCard = function() {
     if ($scope.seenAllZeroes === false) {
-      // so we don't have the 'choose zeros first' rule anymore
-      $scope.seenAllZeroes = true;
-    } else {
-
+      $scope.findNextZero();
+    } else if ($scope.seenAllZeroes === true && $scope.seenAllWithinTime === false) {
+      $scope.findNextWhateverCard();
+    } else if ($scope.seenAllZeroes === true && $scope.seenAllWithinTime === true) {
+      console.log('all done!');
     }
-    return;
-  }
-
-  // // review controller
-  // $scope.reviewTime = function() {
-  //   console.log('inside game');
-
-    // 1. sort the deck by bin
-    // $scope.cards.sort(function (a,b) {
-    //   return b.bin - a.bin;
-    // });
-
-  //   // 2. review all the zero bin cards
-  //   $scope.goThroughDeck(function (card) {
-  //     if (card['bin'] === 0 && $scope.checkTime(card)) {
-  //       // show the card
-  //       $scope.reviewCard(card);
-  //     }
-  //   });
-  // }
+  };
 
   // get the cards from database
   $scope.$on('$viewContentLoaded', function(){
